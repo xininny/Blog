d578dcf82e33962a1c2e3d386ac06f194c420118
xininny <xininny@gmail.com>
2024-06-14 15:50:12 +0900

    profile

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "xininny",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/xininny.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "Security Engineer",
     bio: "Epilogue",
     email: "xininny@gmail.com",
