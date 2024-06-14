a5e775383f51c121011f53046c89afbf84105a02
xininny <xininny@gmail.com>
2024-06-14 23:41:03 +0900

    backdrop

--- src/layouts/RootLayout/Header/index.tsx
+++ src/layouts/RootLayout/Header/index.tsx
@@ -28,7 +28,7 @@ const StyledWrapper = styled.div`
   z-index: ${zIndexes.header};
   position: sticky;
   padding: 10px 0;
-  backdrop-filter: blur(100px);
+  opacity: 1;
   top: 0;
   background-color: ${({ theme }) => theme.colors.grayA1};
   box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
