dc3fe2fc02f092dc63881fb0e3af715de44c7636
xininny <xininny@gmail.com>
2024-06-14 23:27:58 +0900

    blus

--- src/layouts/RootLayout/Header/index.tsx
+++ src/layouts/RootLayout/Header/index.tsx
@@ -28,6 +28,7 @@ const StyledWrapper = styled.div`
   z-index: ${zIndexes.header};
   position: sticky;
   padding: 10px 0;
+  backdrop-filter: blur(50px);
   top: 0;
   background-color: ${({ theme }) => theme.colors.grayA1};
   box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
