d2a0fb48c7ce77ab17ec853b40d97600ca11340e
xininny <xininny@gmail.com>
2024-06-14 23:22:32 +0900

    header color

--- src/layouts/RootLayout/Header/index.tsx
+++ src/layouts/RootLayout/Header/index.tsx
@@ -29,7 +29,7 @@ const StyledWrapper = styled.div`
   position: sticky;
   padding: 10px 0;
   top: 0;
-  background-color: ${({ theme }) => theme.colors.gray2};
+  background-color: ${({ theme }) => theme.colors.grayA1};
   box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
 
   .container {
