be3fe570020ed6399ca4f7cfc3c6c1a0cd9a5d3f
xininny <xininny@gmail.com>
2024-06-14 18:28:13 +0900

    emoji

--- site.config.js
+++ site.config.js
@@ -20,13 +20,13 @@ const CONFIG = {
   blog: {
     title: "xininny",
     description: "xininny's blog",
-    theme: ['light']
+    theme: 'light'
   },
 
   // CONFIG configration (required)
   link: "https://xininny.vercel.app/",
   since: 2024, // If leave this empty, current year will be used.
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
   seo: {
     keywords: ["xininny", "Blog"]
