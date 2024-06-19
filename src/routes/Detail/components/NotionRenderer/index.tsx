4c9b1e91b8999756882a302f5da91897e46f319e
xininny <xininny@gmail.com>
2024-06-20 00:47:42 +0900

    pretendard

--- src/routes/Detail/components/NotionRenderer/index.tsx
+++ src/routes/Detail/components/NotionRenderer/index.tsx
@@ -15,6 +15,7 @@ import "prismjs/themes/prism-tomorrow.css"
 import "katex/dist/katex.min.css"
 import { FC } from "react"
 import styled from "@emotion/styled"
+import { pretendard } from "src/assets"
 
 const _NotionRenderer = dynamic(
   () => import("react-notion-x").then((m) => m.NotionRenderer),
@@ -85,5 +86,11 @@ const StyledWrapper = styled.div`
   }
   .notion-page {
     padding: 0;
+  }
+  .notion {
+    font-family: ${pretendard.style.fontFamily};
+    color: ${({theme}) =>
+      theme === "dark" ? "rgb(209 213 219)" : "rgb(107 114 128);"};
+    overflow-wrap: break-word;
   }
 `
