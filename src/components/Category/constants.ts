ff43e3ff64d13742a8994b4696b4ce56db647d48
xininny <xininny@gmail.com>
2024-07-05 10:19:10 +0900

    category

--- src/components/Category/constants.ts
+++ src/components/Category/constants.ts
@@ -22,7 +22,7 @@ export const CUSTOM_COLOR_SET: Record<string, string> = {
   "🍎 Project":"rgb(255, 216, 216)",
   "✏️ Study":"rgb(251, 239, 186)",
   "🪪 Credential": "rgb(186 230 253)",
-  "🏕️ BootCamp" : "rgb(184 251 223)"
+  "🏕️ BootCamp" : "rgb(184 251 223)",
   "🌐 Extracurriculars" : "rgb(186 230 253)"
   // Add more categories and colors as needed
 };
